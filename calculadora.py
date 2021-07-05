d92bf8780236ca66931d708c2ecc0a0bb41064c3
jsassis <86925244+jsassis@users.noreply.github.com>
2021-07-04 22:37:28 -0300

    Create calculadora.py
    Código

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+#Autor: Jéssica Silva de Assis.
+
+from time import sleep
+from math import ceil
+
+print('-=-'*18)
+print('CALCULE AQUI O ALCANCE DO SEU ANÚNCIO!')
+investimento = float(input('Quanto será investido? R$'))
+print('Analisando as informações...')
+sleep(2)
+
+    #Cálculos
+'''Visualizações iniciais'''
+visualizacao1 = investimento * 30
+'''Total de cliques'''
+clique = visualizacao1 * 0.12
+'''Total de compartilhamentos'''
+compartilhamento = (clique * 0.15) / 4
+'''Visualizações secundárias'''
+visualizacao2 = compartilhamento * 40
+'''Alcance total'''
+alcance = (investimento * 30) + (visualizacao1 * 0.12) + (clique * 0.15) + (compartilhamento * 40)
+
+print('Aproximadamente {} pessoas visualizarão o anúncio.'.format(ceil(alcance)))
+print('-=-'*18)
